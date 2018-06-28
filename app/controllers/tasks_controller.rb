b294030ea63394e65477a795692b1857f2527abf
katsufumi.moriya <moriya_ku@yahoo.co.jp>
2018-06-28 09:38:17 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :edit , :update]
   
   def index
     if logged_in?
@@ -32,11 +32,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    
   end
 
   def update
-    @task = Task.find(params[:id])
+    
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
